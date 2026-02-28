b030f0e44972a2a161fd55ae836da64444634140
pishchela <pishchela.aleksandr@gmail.com>
2026-02-27 21:02:16 -0500

    get data from jsonplaceholder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'remote',
+        loadChildren: () => loadRemoteModule('mocking-data-in-microfrontend-remote', './routes').then(m => m.routes)
+    },
+    {
+        path: '',
+        redirectTo: 'remote',
+        pathMatch: 'full'
+    }
+];
